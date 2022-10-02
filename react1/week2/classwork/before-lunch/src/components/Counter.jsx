f4174f7d0430d48d4452d1ea95d273e206a8d0ed
r0hitsharma <15rohitsharma@gmail.com>
2022-10-02 16:17:43 +0200

    Automatic counters with useEffect.

--- react1/week2/classwork/before-lunch/src/components/Counter.jsx
+++ react1/week2/classwork/before-lunch/src/components/Counter.jsx
@@ -1,20 +1,34 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 export default function Counter ({
-  limit = 5
+  limit = 60,
+  paused,
 }) {
   console.log('render');
   const [count, setCount] = useState(0);
 
-  const disabled = count === limit;
-  console.log({ disabled });
+  const disabled = count >= limit;
+  // console.log({ disabled });
+
+  useEffect(() => {
+    console.log('effect');
+
+    const interval = setInterval(() => {
+      console.log('in effect interval', count);
+      if(!paused)
+        setCount(c => c + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [paused]);
 
   function handleClick () {
     setCount(count + 1);
-    console.log('in handler', count);
+    // console.log('in handler', count);
   }
 
-  console.log('in render', count);
+  // console.log('in render', count);
 
   return (
     <li>
--- react1/week2/classwork/before-lunch/src/components/CounterList.jsx
+++ react1/week2/classwork/before-lunch/src/components/CounterList.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import Counter from './Counter';
 
 const counters = [
-  { id: '1', limit: 3 },
+  { id: '1', limit: 30 },
   { id: '2'}
 ];
 
 export default function CounterList (){
   const [counterList, setCounterList] = useState(counters);
+  const [paused, setPaused] = useState(false);
 
   function addCounter () {
     setCounterList(counterList.concat([{ id: '' + (counterList.length + 1) }]));
@@ -18,8 +19,9 @@ export default function CounterList (){
   return (
     <>
       <button onClick={addCounter}>Add counter!</button>
+      <button onClick={() => setPaused(!paused)}>{paused ? 'Play' : 'Pause'}</button>
       <ul>
-        {counterList.map(({ limit, id }) => <Counter limit={limit} key={id} />)} 
+        {counterList.map(({ limit, id }) => <Counter limit={limit} key={id} paused={paused} />)} 
       </ul>
     </>
   )
